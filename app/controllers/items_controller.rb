c5b14c095db0919193ad5a34a1832b91ee9c96eb
Takuya7897 <69108886+Takuya7897@users.noreply.github.com>
2020-09-04 14:59:34 +0900

    deviseに関するビューファイルの配置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,30 @@
 class ItemsController < ApplicationController
-  
+  def index
+  end
+
+  def new
+    @items= Items.find(params[:id])
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+   
+  end
+
+  def show
+  end
+
+  private
+
+  def items_params
+  end
+
 end
