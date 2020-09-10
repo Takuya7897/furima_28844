3ddc5dd3d9bebd83be85988b7936077d6425930a
Takuya7897 <69108886+Takuya7897@users.noreply.github.com>
2020-09-10 22:03:50 +0900

    ユーザー管理機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,15 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
-    validates :first_name_kana
-    validates :last_name_kana
     validates :nickname
   end
 
   validates :date_of_birth, presence: true
 
-  # validates :email, format: { with:, message: "は半角英数で入力してください。"}
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
